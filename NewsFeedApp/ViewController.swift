b330a568b12d2b0abb353443a5b352993ecbf050
Muneerkk Muneer <muneer@muneers-macbook-pro.local>
2018-10-05 23:43:00 +0530

    Initial Commit

---
+++ NewsFeedApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NewsFeedApp
+//
+//  Created by Muneer KK on 05/10/18.
+//  Copyright © 2018 Muneer KK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
